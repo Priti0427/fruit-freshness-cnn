6b0b70db07efd9d0a98ca9607ac15d2fc5cf5495
Simon T. Rubinstein <102595769+a19simru@users.noreply.github.com>
2026-08-22 10:08:39 -0400

    Create fruit.py

---
+++ fruit.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn as nn
+from torchvision.models import efficientnet_b0, EfficientNet_B0_Weights
+
+weights = EfficientNet_B0_Weights.DEFAULT
+model = efficientnet_b0(weights=weights)
+
+# Freeze pretrained layers
+for param in model.features.parameters():
+    param.requires_grad = False
+
+# Replace classifier
+num_features = model.classifier[1].in_features
+
+model.classifier = nn.Sequential(
+    nn.Dropout(0.3),
+    nn.Linear(num_features, 2)
